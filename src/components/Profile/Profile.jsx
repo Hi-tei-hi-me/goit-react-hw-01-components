3a579f52322530947528dd975960b3920fbd7c31
Hi-tei-hi-me <lethargium@gmail.com>
2022-11-11 00:06:45 +0200

    Task 1 completed

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,28 @@
+import './profile.css';
+
+export const Profile = props => {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img src={props.avatar} alt="User avatar" className="avatar" />
+        <p className="name">{props.username}</p>
+        <p className="tag">@{props.tag}</p>
+        <p className="location">{props.location}</p>
+      </div>
+      <ul className="stats">
+        <li className="stats-block">
+          <span className="label">Followers</span>
+          <span className="quantity">{props.stats.followers.toLocaleString()}</span>
+        </li>
+        <li className="stats-block">
+          <span className="label">Views</span>
+          <span className="quantity">{props.stats.views.toLocaleString()}</span>
+        </li>
+        <li className="stats-block">
+          <span className="label">Likes</span>
+          <span className="quantity">{props.stats.likes.toLocaleString()}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
