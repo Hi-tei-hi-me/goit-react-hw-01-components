125d8896e4b7daf57cd76f8969b5cd420e9325f0
Hi-tei-hi-me <lethargium@gmail.com>
2022-12-18 18:28:04 +0200

    Update getRandomColor.js

--- src/utils/getRandomColor.js
+++ src/utils/getRandomColor.js
@@ -1,3 +1,4 @@
 export const getRandomColor = () => {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  return randomColor.toLowerCase() === '#ffffff' ? getRandomColor() : randomColor;
 };
